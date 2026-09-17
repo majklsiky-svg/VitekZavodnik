2b08252b66bdf1174fb97faeb80dcc4b84e33da8
majklsiky-svg <majklsiky@gmail.com>
2026-09-17 22:27:16 +0200

    Switch runtime to mineral exploration renderer

--- app/src/main/java/cz/vitekzavodnik/game/GameSurfaceView.java
+++ app/src/main/java/cz/vitekzavodnik/game/GameSurfaceView.java
@@ -4,20 +4,19 @@ import android.content.Context;
 import android.opengl.GLSurfaceView;
 
 public final class GameSurfaceView extends GLSurfaceView {
-    private final UltraRenderer renderer;
+    private final SafeGameRenderer renderer;
 
     public GameSurfaceView(Context context) {
         super(context);
         setEGLContextClientVersion(2);
-        setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+        setEGLConfigChooser(8,8,8,8,24,0);
         setPreserveEGLContextOnPause(true);
-        renderer = new UltraRenderer(context);
+        renderer = new SafeGameRenderer(context);
         setRenderer(renderer);
         setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
     }
 
-    public UltraRenderer renderer(){ return renderer; }
-
+    public SafeGameRenderer renderer(){ return renderer; }
     public void resetGame(){ queueEvent(renderer::resetGame); }
     public void setGamePaused(boolean paused){ queueEvent(() -> renderer.setPaused(paused)); }
     public void releaseGame(){ queueEvent(renderer::release); }
