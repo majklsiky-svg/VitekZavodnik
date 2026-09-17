c93194701358cccdea2b822b7d99edc8dff2af1f
majklsiky-svg <majklsiky@gmail.com>
2026-09-17 21:35:41 +0200

    Switch game to premium 3D renderer

--- app/src/main/java/cz/vitekzavodnik/game/GameSurfaceView.java
+++ app/src/main/java/cz/vitekzavodnik/game/GameSurfaceView.java
@@ -4,19 +4,19 @@ import android.content.Context;
 import android.opengl.GLSurfaceView;
 
 public final class GameSurfaceView extends GLSurfaceView {
-    private final SafeGameRenderer renderer;
+    private final PremiumGameRenderer renderer;
 
     public GameSurfaceView(Context context) {
         super(context);
         setEGLContextClientVersion(2);
         setEGLConfigChooser(8, 8, 8, 8, 16, 0);
         setPreserveEGLContextOnPause(true);
-        renderer = new SafeGameRenderer(context);
+        renderer = new PremiumGameRenderer(context);
         setRenderer(renderer);
         setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
     }
 
-    public SafeGameRenderer renderer(){ return renderer; }
+    public PremiumGameRenderer renderer(){ return renderer; }
 
     public void resetGame(){ queueEvent(renderer::resetGame); }
     public void setGamePaused(boolean paused){ queueEvent(() -> renderer.setPaused(paused)); }
