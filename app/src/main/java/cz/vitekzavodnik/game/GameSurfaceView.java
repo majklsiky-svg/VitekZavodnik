3cf1d4de1d8903311c93f0655abe423641c72e27
majklsiky-svg <majklsiky@gmail.com>
2026-09-17 22:00:03 +0200

    Switch game to ultra renderer

--- app/src/main/java/cz/vitekzavodnik/game/GameSurfaceView.java
+++ app/src/main/java/cz/vitekzavodnik/game/GameSurfaceView.java
@@ -4,19 +4,19 @@ import android.content.Context;
 import android.opengl.GLSurfaceView;
 
 public final class GameSurfaceView extends GLSurfaceView {
-    private final PremiumGameRenderer renderer;
+    private final UltraRenderer renderer;
 
     public GameSurfaceView(Context context) {
         super(context);
         setEGLContextClientVersion(2);
         setEGLConfigChooser(8, 8, 8, 8, 16, 0);
         setPreserveEGLContextOnPause(true);
-        renderer = new PremiumGameRenderer(context);
+        renderer = new UltraRenderer(context);
         setRenderer(renderer);
         setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
     }
 
-    public PremiumGameRenderer renderer(){ return renderer; }
+    public UltraRenderer renderer(){ return renderer; }
 
     public void resetGame(){ queueEvent(renderer::resetGame); }
     public void setGamePaused(boolean paused){ queueEvent(() -> renderer.setPaused(paused)); }
